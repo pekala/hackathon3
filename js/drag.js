3e67e637ac9ca024f3c1d08a19328eb43eaac202
Maciej Pekala <mp@issuu.com>
2015-11-23 19:59:33 +0100

    Adds some dragging WIP

---
+++ js/drag.js
@@ -0,0 +1,52 @@
+(function() {
+    var body = document.body,
+        dropArea = document.getElementById( 'drop-area' ),
+        droppableArr = [], dropAreaTimeout;
+
+    // initialize droppables
+    [].slice.call( document.querySelectorAll( '#drop-area .drop-area__item' )).forEach( function( el ) {
+        droppableArr.push( new Droppable( el, {
+            onDrop : function( instance, draggableEl ) {
+                // show checkmark inside the droppabe element
+                classie.add( instance.el, 'drop-feedback' );
+                clearTimeout( instance.checkmarkTimeout );
+                instance.checkmarkTimeout = setTimeout( function() {
+                    classie.remove( instance.el, 'drop-feedback' );
+                }, 800 );
+                // ...
+            }
+        } ) );
+    } );
+
+    // initialize draggable(s)
+    [].slice.call(document.querySelectorAll( '#grid .grid__item' )).forEach( function( el ) {
+        new Draggable( el, droppableArr, {
+            draggabilly : { },
+            onStart : function() {
+                // add class 'drag-active' to body
+                classie.add( body, 'drag-active' );
+                // clear timeout: dropAreaTimeout (toggle drop area)
+                clearTimeout( dropAreaTimeout );
+                // show dropArea
+                classie.add( dropArea, 'show' );
+            },
+            onEnd : function( wasDropped ) {
+                var afterDropFn = function() {
+                    // hide dropArea
+                    classie.remove( dropArea, 'show' );
+                    // remove class 'drag-active' from body
+                    classie.remove( body, 'drag-active' );
+                };
+
+                if( !wasDropped ) {
+                    afterDropFn();
+                }
+                else {
+                    // after some time hide drop area and remove class 'drag-active' from body
+                    clearTimeout( dropAreaTimeout );
+                    dropAreaTimeout = setTimeout( afterDropFn, 400 );
+                }
+            }
+        } );
+    } );
+})();
